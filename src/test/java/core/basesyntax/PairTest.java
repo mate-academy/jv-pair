0619d7effb47659a08e9b372969ebc7334ad8a5a
Bloodstinger <47367238+Bloodstinger@users.noreply.github.com>
2019-08-26 07:08:12 +0300

    Update PairTest.java

--- src/test/java/core/basesyntax/PairTest.java
+++ src/test/java/core/basesyntax/PairTest.java
@@ -1,13 +1,30 @@
 package core.basesyntax;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.lang.reflect.Method;
 
 public class PairTest {
 
-    private Pair firstPair = Pair.of(5, "Five");
-    private Pair secondPair = Pair.of(2, "Two");
-    private Pair thirdPair = Pair.of(5, "Five");
+    private Pair firstPair;
+    private Pair secondPair;
+    private Pair thirdPair;
+
+    @Before
+    public void init() {
+        firstPair = Pair.of(5, "Five");
+        secondPair = Pair.of(2, "Two");
+        thirdPair = Pair.of(5, "Five");
+    }
+
+    @Test
+    public void methodsCheck() {
+        Class<Pair> pairClass = Pair.class;
+        Method[] methods = pairClass.getMethods();
+        Assert.assertTrue("Not all methods were implemented", methods.length >= 5);
+    }
 
     @Test
     public void pairsAreEquals() {
@@ -20,31 +37,20 @@ public class PairTest {
     }
 
     @Test
-    public void getFirstMethodTest() {
-        Assert.assertEquals("getFirst() method doesn't work correctly",5, firstPair.getFirst());
-        Assert.assertEquals("getFirst() method doesn't work correctly",5, thirdPair.getFirst());
-        Assert.assertNotEquals("getFirst() method doesn't work correctly",1,
-                thirdPair.getFirst());
-        Assert.assertNotEquals("getFirst() method doesn't work correctly",3, thirdPair.getFirst());
-    }
-
-    @Test
-    public void getSecondMethodTest() {
-        Assert.assertEquals("getSecond() method doesn't work correctly","Five",
-                firstPair.getSecond());
-        Assert.assertEquals("getSecond() method doesn't work correctly","Five",
-                thirdPair.getSecond());
-        Assert.assertNotEquals("getSecond() method doesn't work correctly","five",
-                thirdPair.getSecond());
-        Assert.assertNotEquals("getSecond() method doesn't work correctly","five",
-                thirdPair.getSecond());
-    }
-
-    @Test
     public void hashCodeTest() {
         Assert.assertEquals("Hash codes are not equal!", firstPair.hashCode(), thirdPair.hashCode());
         Assert.assertNotEquals("Hash codes are not equal!", firstPair.hashCode(), secondPair.hashCode());
     }
 
+    @Test
+    public void getFirstTest() {
+        Assert.assertEquals(5, firstPair.getFirst());
+        Assert.assertEquals(2, secondPair.getFirst());
+    }
 
+    @Test
+    public void getSecondTest() {
+        Assert.assertEquals("Five", firstPair.getSecond());
+        Assert.assertEquals("Two", secondPair.getSecond());
+    }
 }
