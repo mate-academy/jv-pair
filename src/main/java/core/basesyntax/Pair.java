728747d067fea0ac54d2711f2365e97183cd5f4e
Bloodstinger <47367238+Bloodstinger@users.noreply.github.com>
2019-08-26 21:08:21 +0300

    Update Pair.java

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,40 +1,5 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
-class Pair<T, K> {
-    private final T first;
-    private final K second;
-
-    private Pair(T first, K second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    public static <T, K> Pair<T, K> of(T firstValue, K secondValue) {
-        return new Pair<>(firstValue, secondValue);
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Pair<?, ?> pair = (Pair<?, ?>) o;
-        return Objects.equals(getFirst(), pair.getFirst()) &&
-                Objects.equals(getSecond(), pair.getSecond());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getFirst(), getSecond());
-    }
-
-    public T getFirst() {
-        return this.first;
-    }
-
-    public K getSecond() {
-        return this.second;
-    }
+class Pair {
 
 }
