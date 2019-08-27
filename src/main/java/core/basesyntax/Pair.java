739c4c62674d193c7d2bcc17b145e05f36e1e198
roman_minevich <283389dv3qsh>
2019-08-27 17:13:57 +0300

    Added solution

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.Objects;
 
+class Pair <K, V> {
+    private K firstArgument;
+    private V secondArgument;
+
+    private Pair(K firstArgument, V secondArgument) {
+        this.firstArgument = firstArgument;
+        this.secondArgument = secondArgument;
+    }
+
+    public static <K, V> Pair <K, V> of(K firstArgument, V secondArgument) {
+        return new Pair<>(firstArgument, secondArgument);
+    }
+    public K getFirst() {
+        return firstArgument;
+    }
+
+    public V getSecond() {
+        return secondArgument;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object instanceof Pair) {
+            Pair <K, V> pairObject = (Pair) object;
+            if(this.firstArgument.equals(pairObject.firstArgument) &&
+                    Objects.equals(this.secondArgument, pairObject.secondArgument))
+                return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstArgument, secondArgument);
+    }
 }
