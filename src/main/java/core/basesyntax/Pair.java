02d2e7e767bcc64be9d872e006ac2190ef7d3bff
joej0e <36197513+joej0e@users.noreply.github.com>
2019-08-27 14:13:16 +0300

    Update Pair.java

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,58 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.NoSuchElementException;
+import java.util.Objects;
+
+class Pair<T, V> {
+    private T firstValue;
+    private V secondValue;
+
+    public T getFirst() {
+        if (firstValue == null) {
+            throw new NoSuchElementException("No value present");
+        }
+        return firstValue;
+    }
+
+    public V getSecond() {
+        if (secondValue == null) {
+            throw new NoSuchElementException("No value present");
+        }
+        return secondValue;
+    }
+
+    private Pair() {
+        this.firstValue = null;
+        this.secondValue = null;
+    }
+
+    private Pair(T firstValue, V secondValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+    }
+
+
+    public static <T, V> Pair<T, V> of(T firstValue, V secondValue) {
+
+        return new Pair<>(firstValue, secondValue);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof Pair)) {
+            return false;
+        }
+
+        Pair<?, ?> other = (Pair<?, ?>) obj;
+        return Objects.equals(firstValue, other.firstValue) && Objects.equals(secondValue, other.secondValue);
+    }
+
+    public int hashCode() {
+        return Objects.hash(firstValue, secondValue);
+    }
 
 }
