deee21746cc9e9402a2e23f2ec2a10222f6d504f
Taranarnik <pavelkurilyuk97@gmail.com>
2019-08-27 13:42:00 +0300

    added solution for pair homework

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,40 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, K> {
+    T first;
+    K second;
 
+    private Pair(T first, K second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <T, K> Pair<T, K> of(K first, T second) {
+        return new Pair(first, second);
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public K getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return first == pair.first && second == pair.second;
+    }
+
+    @Override
+    public int hashCode() {
+        return first.hashCode() ^ second.hashCode() * 31;
+    }
 }
