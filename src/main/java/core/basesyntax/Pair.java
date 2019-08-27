6a6a52df5c61058770c870b6680e61dfe6966db0
RostykRosolovskyi <38504193+RostykRosolovskyi@users.noreply.github.com>
2019-08-27 14:35:30 +0300

    realilze class pair

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.Objects;
 
+class Pair<T, N> {
+    private T firstType;
+    private N secondType;
+
+    public Pair(T firstType, N secondType) {
+        this.firstType = firstType;
+        this.secondType = secondType;
+    }
+
+    public static <T, N> Pair of(T first, N second) {
+        return new Pair(first, second);
+    }
+
+    public T getFirst() {
+        return firstType;
+    }
+
+    public N getSecond() {
+        return secondType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(firstType, pair.firstType)
+                && Objects.equals(secondType, pair.secondType);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstType, secondType);
+    }
 }
