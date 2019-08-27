4e38b078bb3e7d9933edce41eb6ab215cc71ccf1
Тарас <tarashryts@gmail.com>
2019-08-27 15:31:42 +0300

    add hw-61-pair

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,42 @@
 package core.basesyntax;
 
-class Pair {
 
+class Pair<T, V> {
+    private T first;
+    private V second;
+
+    public static <T, V> Pair of(T t, V v) {
+        return new Pair(t, v);
+    }
+
+    public Pair(T t, V v) {
+        this.first = t;
+        this.second = v;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<T, V> pair = (Pair) o;
+        return pair.getFirst().equals(this.first)
+                && pair.getSecond().equals(this.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * first.hashCode() + second.hashCode();
+    }
 }
