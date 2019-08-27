6abbd892914ffae2d2f6bc4776fcf0b83c7a03dc
Max <skaterweather41@gmail.com>
2019-08-27 13:17:13 +0200

    added solution to task 1 Pair

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,41 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, K> {
+    private T firstValue;
+    private K secondValue;
 
+    private Pair(T firstValue, K secondValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+    }
+
+    public static <T, K> Pair of(T firstValue, K secondValue) {
+        return new Pair<>(firstValue, secondValue);
+    }
+
+    public T getFirst() {
+        return firstValue;
+    }
+
+    public K getSecond() {
+        return secondValue;
+    }
+
+    @Override
+    public int hashCode() {
+        return firstValue.hashCode() + secondValue.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+        if (object instanceof Pair) {
+            Pair pair = (Pair) object;
+            return pair.getFirst().equals(this.firstValue)
+                    && pair.getSecond().equals(this.secondValue);
+        }
+        return false;
+    }
 }
