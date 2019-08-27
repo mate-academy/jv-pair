52ca7adb9f77b3362af541fe79a32042d1fcde85
Kokhanevich <nike.k1995@gmail.com>
2019-08-27 15:13:24 +0300

    added solution for HW6-pair

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,48 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, V> {
+    private T first;
+    private V second;
 
+    private Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    public void setSecond(V second) {
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return this.first.equals(pair.first) && this.second.equals(pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return first.hashCode() + second.hashCode() * 31;
+    }
+
+    public static <T, V> Pair of(T first, V second) {
+        return new Pair<>(first, second);
+    }
 }
