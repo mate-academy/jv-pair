5600fc5be5163a9f325a422d89d1e2045dd2ee8c
Taras Khalak <tarasulo@gmail.com>
2019-08-28 14:37:22 +0300

    'Solution'

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-class Pair {
+class Pair <A, B> {
+    private A first;
+    private B second;
 
+    public Pair(A first, B second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public A getFirst() {
+        return first;
+    }
+
+    public B getSecond() {
+        return second;
+    }
+
+    public void setFirst(A first) {
+        this.first = first;
+    }
+
+    public void setSecond(B second) {
+        this.second = second;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31* first.hashCode() + second.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == this) {
+            return true;
+        }
+        if (object instanceof Pair) {
+            Pair<A, B> pair = (Pair<A, B>) object;
+            return first.equals(pair.first) && second.equals(pair.second);
+        }
+    }
 }
