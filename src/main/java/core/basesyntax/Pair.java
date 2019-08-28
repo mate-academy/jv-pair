e0d6abc751a2dce8957b7bb1b8091d4b03df0721
kuhaj <alex.kukhtin.dev@gmail.com>
2019-08-28 11:45:44 +0300

    realisation pair

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.Objects;
 
+class Pair<T, K> {
+    private T typeFirst;
+    private K typeSecond;
+
+    Pair(T typeFirst, K typeSecond) {
+        this.typeFirst = typeFirst;
+        this.typeSecond = typeSecond;
+    }
+
+    public static <T, K> Pair of(T typeFirst, K typeSecond) {
+        return new Pair(typeFirst, typeSecond);
+    }
+
+    public T getFirst() {
+        return typeFirst;
+    }
+
+    public K getSecond() {
+        return typeSecond;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(typeFirst, typeSecond);
+    }
+
+    @Override
+    public boolean equals(Object myObj) {
+        if (this == myObj) {
+            return true;
+        }
+        if (myObj == null || getClass() != myObj.getClass()) {
+            return false;
+        }
+        Pair<T, K> pair = (Pair) myObj;
+        return pair.typeFirst.equals(this.typeFirst)
+                && pair.typeSecond.equals(this.typeSecond);
+    }
 }
