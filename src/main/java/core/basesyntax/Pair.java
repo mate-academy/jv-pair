c92b85e6100cf6590f4d2a1359cff7b85a59a71c
MrNopi <Smart.like1998@gmail.com>
2019-12-10 17:49:48 +0300

    Finished project

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,40 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, K> {
+    private T firstElement;
+    private K secondElement;
 
+    private Pair(T first, K second) {
+        firstElement = first;
+        secondElement = second;
+    }
+
+    public static <T, K> Pair of(T t, K k) {
+        return new Pair(t, k);
+    }
+
+    public T getFirst() {
+        return firstElement;
+    }
+
+    public K getSecond() {
+        return secondElement;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        Pair obj = (Pair) object;
+        if (firstElement == obj.getFirst()) {
+            return secondElement.equals(obj.getSecond());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * getFirst().hashCode() + (31 * getSecond().hashCode());
+    }
 }
