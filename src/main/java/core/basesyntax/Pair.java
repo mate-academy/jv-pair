789f366bc301627555794a085dc2e6080e5e01b9
Misha <mishabeheka@gmail.com>
2019-12-10 16:43:51 +0200

    DoneHW_6.1

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,45 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<F, S> {
+
+    public static <F, S> Pair of(F first, S second) {
+        return new Pair<>(first, second);
+    }
+
+    F first;
+    S second;
+
+    private Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public int hashCode() {
+        int prime = 31;
+        int result = 1;
+        result = prime + result * first.hashCode();
+        result = prime + result * second.hashCode();
+        return result;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (this.getClass() != obj.getClass() || obj == null) {
+            return false;
+        }
+
+        Pair pair = (Pair) obj;
+        return first.equals(pair.first) && second.equals(pair.second);
+    }
 
 }
