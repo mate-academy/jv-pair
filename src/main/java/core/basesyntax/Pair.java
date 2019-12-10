adca84864a0820275f1796dfdd6b3282f460a659
Taraban <Taraban.dmitry@gmail.com>
2019-12-10 16:38:08 +0200

    added pair solution

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,47 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, K> {
 
+    private T value;
+    private K value2;
+
+    private Pair(T value, K value2) {
+        this.value = value;
+        this.value2 = value2;
+    }
+
+    public T getFirst() {
+        return value;
+    }
+
+    public K getSecond() {
+        return value2;
+    }
+
+    public static <T, K> Pair of(T value, K value2) {
+        return new Pair<>(value, value2);
+    }
+
+    @Override
+    public boolean equals(Object objToCompare) {
+
+        if (objToCompare == null) {
+            return false;
+        }
+        if (this == objToCompare) {
+            return true;
+        }
+        if (objToCompare.getClass() != Pair.class) {
+            return false;
+        }
+        Pair pair = (Pair) objToCompare;
+        return this.value.equals(pair.value)
+                && this.value2.equals(pair.value2);
+    }
+
+    @Override
+    public int hashCode() {
+        return ((value != null) ? value.hashCode() : 0) * 31
+                + ((value2 != null) ? value2.hashCode() : 0) * 31;
+    }
 }
