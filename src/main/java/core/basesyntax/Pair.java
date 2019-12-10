fdaf76154ccda0c8aeacff104e77e917a030462c
ElvenNurse <58395217+ElvenNurse@users.noreply.github.com>
2019-12-10 15:04:33 +0200

    Added solution for hw 6.1

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,48 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, K> {
+    T firstItem;
+    K secondItem;
 
+    public T getFirst() {
+        return firstItem;
+    }
+
+    public K getSecond() {
+        return secondItem;
+    }
+
+    private Pair(T firstItem, K secondItem) {
+        this.firstItem = firstItem;
+        this.secondItem = secondItem;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+
+        if (firstItem != null ? !firstItem.equals(pair.firstItem) : pair.firstItem != null) {
+            return false;
+        }
+        return secondItem != null ? secondItem.equals(pair.secondItem) : pair.secondItem == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = firstItem != null ? firstItem.hashCode() : 0;
+        result = 31 * result + (secondItem != null ? secondItem.hashCode() : 0);
+        return result;
+    }
+
+    public static Pair of(Object first, Object second) {
+        Pair pair = new Pair(first, second);
+        return pair;
+    }
 }
