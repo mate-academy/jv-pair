6868f0d5fef454f83823a24019f184ebe594ce83
Yaroslava Bondarchuk <yaroslava7bondarchuk@gmail.com>
2019-12-10 12:52:49 +0200

    added generic class Pair

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, K> {
+    private T first;
+    private K second;
 
+    private Pair(T first, K second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public K getSecond() {
+        return second;
+    }
+
+    public static <T, K> Pair of(T first, K second) {
+        return new Pair <>(first, second);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * first.hashCode() * second.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Pair pair = (Pair) obj;
+        return ((first == pair.getFirst() || (first != null
+                && first.equals(pair.getFirst())))
+                && (second == pair.getSecond() || (second != null
+                && second.equals(pair.getSecond()))));
+    }
 }
