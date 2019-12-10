928a861c3ba2994cf5b3874649a556738500bec2
Mykhailo Kramar <3805033@gmail.com>
2019-12-10 16:25:21 +0200

    hw6.1

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,40 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<I, S> {
+    private I first;
+    private S second;
 
+    public static <I, K> Pair of(I first, K second) {
+        return new Pair<>(first, second);
+    }
+
+    private Pair(I first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair pair = (Pair) o;
+        return this.second == pair.second & this.first == pair.first;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * first.hashCode() + second.hashCode();
+    }
+
+    public I getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
 }
