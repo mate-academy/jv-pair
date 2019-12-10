bab8987252b160a077652fe137e6eef59aa8f807
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2019-12-10 13:35:38 +0200

    added some changes

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -19,7 +19,7 @@ class Pair<K, V> {
         return this.second;
     }
 
-    static <K, V> Pair of(K key, V value) {
+    public static <K, V> Pair of(K key, V value) {
         return new Pair<>(key, value);
     }
 
