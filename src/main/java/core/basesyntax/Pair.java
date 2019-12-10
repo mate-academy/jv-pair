f265fe440d88493298eab1c0a7da7b0a740c5348
IvanKrivoruchko <krivoruchko.ivan.ukr.net>
2019-12-10 23:44:44 +0200

    Added hw 61 solution

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,46 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<K, N> {
+    private K firstElement;
+    private N secondElement;
 
+    private Pair(K firstElement, N secondElement) {
+        this.firstElement = firstElement;
+        this.secondElement = secondElement;
+    }
+
+    public K getFirst() {
+        return firstElement;
+    }
+
+    public N getSecond() {
+        return secondElement;
+    }
+
+    public static <K, N> Pair of(K first, N second) {
+        return new Pair<>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair pair = (Pair) o;
+        return (firstElement == pair.firstElement
+                || (firstElement != null && firstElement.equals(pair.getFirst())))
+                && (secondElement == pair.secondElement
+                || (secondElement != null && secondElement.equals(pair.getSecond())));
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + (firstElement != null ? firstElement.hashCode() : 0);
+        result = 31 * result + (secondElement != null ? secondElement.hashCode() : 0);
+        return result;
+    }
 }
