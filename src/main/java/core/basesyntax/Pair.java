66becb2da2430d8c010a522ae891412e1888ecc4
levko <levkosvd@gmail.com>
2019-12-10 19:28:47 +0200

    hm6.1 v1

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,41 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T,K> {
+    private T firstObject;
+    private K secondObject;
 
+    private Pair(T firstObject, K secondObject) {
+        this.firstObject = firstObject;
+        this.secondObject = secondObject;
+    }
+
+    public static <T,K> Pair of(T localFirst, K localSecond) {
+        return new Pair(localFirst, localSecond);
+    }
+
+    public T getFirst() {
+        return firstObject;
+    }
+
+    public K getSecond() {
+        return secondObject;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * (firstObject.hashCode() + secondObject.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+        Pair localObject = (Pair)object;
+        if (localObject.firstObject.equals(this.firstObject)
+                && localObject.secondObject.equals(this.secondObject)) {
+            return true;
+        }
+        return false;
+    }
 }
