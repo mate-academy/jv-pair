5a2b1fa557bd582f13c1ec12c97d3c574f75cf9e
Dmytro Hryhoruk <dimamyhgrygoruk@gmail.com>
2019-12-10 12:51:12 +0200

    6.1 is done

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.Objects;
 
+class Pair<K, V> {
+    private K first;
+    private V second;
+
+    private Pair(K first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public K getFirst() {
+        return this.first;
+    }
+
+    public V getSecond() {
+        return this.second;
+    }
+
+    static <K, V> Pair of(K key, V value) {
+        return new Pair<>(key, value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<K, V> pair = (Pair<K, V>) o;
+        return Objects.equals(first, pair.first)
+                && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
 }
