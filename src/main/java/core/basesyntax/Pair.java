9d7d5638f3d9cff039baac276359e83aaad98b03
Oleksandr Husenko <oleksandrhusenko@gmail.com>
2020-04-05 22:45:44 +0300

    initial commit (added solution)

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,50 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<K, V> {
+    private K key;
+    private V value;
 
+    private Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    static <K, V> Pair of(K key, V value) {
+        return new Pair(key, value);
+    }
+
+    K getFirst() {
+        return key;
+    }
+
+    V getSecond() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (obj.getClass().equals(getClass())) {
+            Pair pair = (Pair) obj;
+            return this.hashCode() == pair.hashCode();
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        if (key != null) {
+            result = 19 * result + key.hashCode();
+        }
+        if (value != null) {
+            result = 23 * result + value.hashCode();
+        }
+        return result;
+    }
 }
