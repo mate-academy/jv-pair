180c31f52f1b60458010407534f5a519cbc09f6b
Serhii Maksymov <serhio.maksymov@gmail.com>
2020-04-06 18:09:43 +0300

    added solution hw-6.2.1

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,47 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, S> {
+    private T first;
+    private S second;
 
+    private Pair(T first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    static <T, S> Pair of(T t, S s) {
+        return new Pair(t, s);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Pair<T, S> pair = (Pair<T, S>) obj;
+
+        if (first != null ? !first.equals(pair.first) : pair.first != null) {
+            return false;
+        }
+        return second != null ? second.equals(pair.second) : pair.second == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (second != null ? second.hashCode() : 0);
+        return result;
+    }
 }
