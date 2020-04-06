ac25abe803654454e100270d11061d781264aaf5
Vlad Petrovskyi <62035381+vladpetrovskyi@users.noreply.github.com>
2020-04-06 17:28:21 +0300

    Update src/main/java/core/basesyntax/Pair.java
    Co-Authored-By: RomanDubovskyi <58385506+RomanDubovskyi@users.noreply.github.com>

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -11,7 +11,7 @@ class Pair<T1, T2> {
         this.second = second;
     }
 
-    public static <T1, T2> Pair<T1, T2> of(T1 t1, T2 t2) {
+   public static <T1, T2> Pair of(T1 first, T2 second) {
         return new Pair<>(t1, t2);
     }
 
