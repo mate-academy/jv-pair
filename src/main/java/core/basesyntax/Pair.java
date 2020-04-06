f4d131d27982ac0034f2c5fd63c0c7bedb6e2a60
Popovych Yurii <popovychyb@gmail.com>
2020-04-06 17:04:28 +0300

    added solution

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.Objects;
 
+class Pair<T, S> {
+    private T first;
+    private S second;
+
+    private Pair(T first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <T, S> Pair of(T first, S second) {
+        return new Pair(first, second);
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first)
+                && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(first, second);
+    }
 }
