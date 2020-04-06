046c3a31bc2ae13a8a1a1cb51b730bcc7e756f8f
Nick97-git <nickarchangel97@gmail.com>
2020-04-06 13:02:49 +0300

    added solution

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,60 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, E> {
+    private T firstElement;
+    private E secondElement;
 
+    private Pair(T firstElement, E secondElement) {
+        this.firstElement = firstElement;
+        this.secondElement = secondElement;
+    }
+
+    public static <T, E> Pair of(T firstElement, E secondElement) {
+        return new Pair<>(firstElement, secondElement);
+    }
+
+    public T getFirst() {
+        return firstElement;
+    }
+
+    public E getSecond() {
+        return secondElement;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+
+        if (o == null) {
+            return false;
+        }
+
+        if (getClass() == o.getClass()) {
+            Pair<?, ?> pair = (Pair<?, ?>) o;
+            return compareElements(pair.firstElement, this.firstElement)
+                    && compareElements(pair.secondElement, this.secondElement);
+        }
+
+        return false;
+    }
+
+    private boolean compareElements(Object firstElement, Object secondElement) {
+        if (firstElement == null && this.secondElement == null) {
+            return true;
+        }
+
+        if (firstElement == null || this.secondElement == null) {
+            return false;
+        }
+
+        return firstElement.equals(secondElement);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 + 15 * (this.firstElement == null ? 0 : firstElement.hashCode())
+                + 8 * (this.secondElement == null ? 0 : secondElement.hashCode());
+    }
 }
