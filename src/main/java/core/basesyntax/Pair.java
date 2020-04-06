fbf4600d561324452f217a68de2928770418bd80
maks45 <maksim.durov45@gmail.com>
2020-04-06 15:02:20 +0300

    solution added & fixed code style errors

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,44 +1,44 @@
 package core.basesyntax;
 
 class Pair<T, S> {
-    private T t;
-    private S s;
+    private T fist;
+    private S second;
 
-    public Pair(T t, S s) {
-        this.t = t;
-        this.s = s;
+    public Pair(T fist, S second) {
+        this.fist = fist;
+        this.second = second;
     }
 
-    public T getFirst(){
-        return t;
+    public T getFirst() {
+        return fist;
     }
 
-    public S getSecond(){
-        return s;
+    public S getSecond() {
+        return second;
     }
 
-    public static <T,K> Pair<T,K> of(T t, K k){
-        return new Pair<>(t,k);
+    public static <T, K> Pair<T, K> of(T t, K k) {
+        return new Pair<>(t, k);
     }
 
     @Override
     public boolean equals(Object obj) {
-        if(this == obj){
+        if (this == obj) {
             return true;
         }
-        if(obj == null || !obj.getClass().equals(getClass())){
+        if (obj == null || !obj.getClass().equals(getClass())) {
             return false;
         }
-        Pair<?,?> pair = (Pair<?,?>) obj;
-        return pair.t != null ? pair.t.equals(t) : t==null
-                && pair.s != null ? pair.s.equals(s) : s==null;
+        Pair<?, ?> pair = (Pair<?, ?>) obj;
+        return pair.fist != null ? pair.fist.equals(fist) : fist == null
+                && pair.second != null ? pair.second.equals(second) : second == null;
     }
 
     @Override
     public int hashCode() {
         int result = 17;
-        result = result * 31 + (t != null ? t.hashCode() : 1);
-        result = result * 31 + (s != null ? s.hashCode() : 1);
+        result = result * 31 + (fist != null ? fist.hashCode() : 1);
+        result = result * 31 + (second != null ? second.hashCode() : 1);
         return result;
     }
 }
