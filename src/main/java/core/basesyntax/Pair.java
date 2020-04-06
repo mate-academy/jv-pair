1c6251e00e825da30c2bbb27899bd3b2f01e413b
Andrew <tolstuy1996@gmail.com>
2020-04-06 17:42:44 +0300

    complete pair

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.Objects;
 
+class Pair<T, K> {
+    private T firstElement;
+    private K secondElement;
+
+    private Pair(T first, K second) {
+        this.firstElement = first;
+        this.secondElement = second;
+    }
+
+    public T getFirst() {
+        return firstElement;
+    }
+
+    public K getSecond() {
+        return secondElement;
+    }
+
+    public static <T, K> Pair of(T first, K second) {
+        return new Pair(first, second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair pair = (Pair) o;
+        return (Objects.equals(firstElement, pair.firstElement))
+                && (Objects.equals(secondElement, pair.secondElement));
+    }
+
+    @Override
+    public int hashCode() {
+        return 41 * firstElement.hashCode() + secondElement.hashCode();
+    }
 }
