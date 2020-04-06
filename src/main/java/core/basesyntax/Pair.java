c00adef805a4a8ceafa84d6fd7741d253ba93b67
woododoCode <woododoCode@users.noreply.github.com>
2020-04-06 15:50:19 +0300

    Solution hw621

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,50 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.Objects;
 
+class Pair<F, S> {
+    private F firstIncome;
+    private S secondIncome;
+
+    private Pair(F firstValue, S secondValue) {
+        this.firstIncome = firstValue;
+        this.secondIncome = secondValue;
+    }
+
+    public static <F, S> Pair<F, S> of(F f, S s) {
+        return new Pair<>(f, s);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) object;
+        return Objects.equals(firstIncome, pair.firstIncome)
+                && Objects.equals(secondIncome, pair.secondIncome);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 7;
+        if (firstIncome != null) {
+            result = 17 * result + firstIncome.hashCode();
+        }
+        if (secondIncome != null) {
+            result = 17 * result + secondIncome.hashCode();
+        }
+        return result;
+    }
+
+    public F getFirst() {
+        return firstIncome;
+    }
+
+    public S getSecond() {
+        return secondIncome;
+    }
 }
