0dd4aeb596ed360dbe21dda741f935b0dbda4376
ana-dav <opa.super.op@gmail.com>
2020-04-06 15:23:26 +0300

    6.2.1

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,42 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.Objects;
 
+class Pair <K, V> {
+    private K key;
+    private V value;
+
+    private Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public static <K, V> Pair<K, V> of(K key, V value) {
+        return new Pair<>(key, value);
+    }
+
+    public K getFirst() {
+        return key;
+    }
+
+    public V getSecond() {
+        return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Pair)) return false;
+        Pair pair = (Pair) o;
+        return Objects.equals(key, ((Pair) o).key) &&
+                Objects.equals(value, ((Pair) o).value);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result *= 31 + key.hashCode();
+        result *= 31 + value.hashCode();
+        return result;
+    }
 }
