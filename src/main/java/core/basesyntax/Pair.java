79a43ecea376e432ce48986b5890d02f99ad88e6
VitaliiLeshchenko <411100253g@gmail.com>
2020-04-06 19:57:46 +0300

    removed Objects.equals

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,7 +1,5 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 class Pair<T, F> {
 
     private T first;
@@ -20,7 +18,7 @@ class Pair<T, F> {
         return this.second;
     }
 
-    public static Pair of(Object o1, Object o2) {
+    public static <T, F> Pair of(Object o1, Object o2) {
         return new Pair(o1, o2);
     }
 
@@ -32,28 +30,16 @@ class Pair<T, F> {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        Pair<?, ?> pair = (Pair<?, ?>) o;
-        return Objects.equals(first, pair.first)
-                && Objects.equals(second, pair.second);
+        Pair<T, F> pair = (Pair) o;
+        return (first == pair.first || first != null && first.equals(pair.first))
+                && (second == pair.second || second != null && second.equals(pair.second));
     }
 
     @Override
     public int hashCode() {
-        Object[] a = {this.first, this.second};
-        if (a == null) {
-            return 0;
-        } else {
-            int result = 1;
-            Object[] var2 = a;
-            int var3 = a.length;
-
-            for (int var4 = 0; var4 < var3; ++var4) {
-                Object element = var2[var4];
-                result = 31 * result + (element == null ? 0 : element.hashCode());
-            }
-            return result;
-        }
+        int result = 17;
+        result = result * 31 + (this.first == null ? 0 : this.first.hashCode());
+        result = result * 31 + (this.second == null ? 0 : this.second.hashCode());
+        return result;
     }
 }
-
-
