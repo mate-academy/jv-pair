acc2c3c8b94eb8f52c117e5aa4d0ce24866fc79b
Superman696 <61060058+Superman696@users.noreply.github.com>
2020-04-06 14:12:57 +0300

    used generics with class and method

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,49 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, P> {
+    private T first;
+    private P second;
 
+    private Pair(T first, P second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public P getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<T, P> obj = (Pair<T, P>) o;
+        return first == null && obj.first == null || first != null && first.equals(obj.first)
+                && second == null && obj.second == null || second != null
+                && second.equals(obj.second);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        if (first != null) {
+            result += 31 * first.hashCode();
+        }
+        if (second != null) {
+            result += 31 * second.hashCode();
+        }
+        return result;
+    }
+
+    public static <T, P> Pair of(T first, P second) {
+        return new Pair<>(first, second);
+    }
 }
