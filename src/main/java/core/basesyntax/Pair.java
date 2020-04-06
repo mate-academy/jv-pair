46792322ee7ef46013b8ebc290bb9623d8c0054f
NastiaFasova <fasovaaa@fmail.com>
2020-04-06 12:45:21 +0300

    Added solution too the 1-st task of HW-7

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,47 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.Objects;
 
+class Pair<T,V> {
+
+    private T first;
+    private V second;
+
+    private Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        return Objects.equals(first, pair.first)
+                && Objects.equals(second, pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = result * 31 + (first == null ? 0 : first.hashCode());
+        result = result * 31 + (second == null ? 0 : second.hashCode());
+        return result;
+    }
+
+    public static <T, V> Pair<T, V> of(T t, V v) {
+        return new Pair<T, V>(t, v);
+    }
 }
