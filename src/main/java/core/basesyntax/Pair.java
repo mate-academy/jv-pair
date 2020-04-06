238a1d0f160dc8226adadb416ae1f5617848d155
Galina <gala6ap@mail.ru>
2020-04-06 17:48:09 +0300

    fixed

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -9,8 +9,16 @@ class Pair<K, T> {
         this.second = second;
     }
 
-    public static <K, T> Pair<K, T> of(K firstValue, T secondValue) {
+    public static <K, T> Pair of(K firstValue, T secondValue) {
         return new Pair<>(firstValue, secondValue);
+    }
+
+    public K getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
     }
 
     @Override
@@ -21,30 +29,22 @@ class Pair<K, T> {
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
-        Pair<K, T> pair = (Pair<K, T>) o;
+        Pair<K, T> pair = (Pair) o;
         return ((first == pair.first)
                 || (first != null && first.equals(pair.first)))
                 && ((second == pair.second)
                 || (second != null && second.equals(pair.second)));
     }
 
-   @Override
+    @Override
     public int hashCode() {
         int value = 17;
         if (first != null) {
-            value = 31 * value +  first.hashCode();
+            value = 31 * value + first.hashCode();
         }
         if (second != null) {
-            value = 31 * value +  second.hashCode();
+            value = 31 * value + second.hashCode();
         }
         return value;
-    }
-
-    public K getFirst() {
-        return first;
-    }
-
-    public T getSecond() {
-        return second;
     }
 }
