87d25468204fdfaa071d47ceb999ff8055f4a81f
Allduiin <vovapro01@gmail.com>
2020-04-06 17:31:43 +0300

    Objects Equals&HashCode --> My Equals&Hashcode

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,14 +1,16 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 class Pair<F, S> {
     F parameter1;
     S parameter2;
 
-    public Pair(F parameter1, S parameter2) {
+    private Pair(F parameter1, S parameter2) {
         this.parameter1 = parameter1;
         this.parameter2 = parameter2;
+    }
+
+    public static <F, S> Pair of(F f, S s) {
+        return new Pair(f, s);
     }
 
     public F getFirst() {
@@ -28,16 +30,17 @@ class Pair<F, S> {
             return false;
         }
         Pair<?, ?> pair = (Pair<?, ?>) o;
-        return Objects.equals(parameter1, pair.parameter1)
-                && Objects.equals(parameter2, pair.parameter2);
+        return ((parameter1 == pair.parameter1)
+                || (parameter1 != null && parameter1.equals(pair.parameter1)))
+                && ((parameter2 == pair.parameter2)
+                || (parameter2 != null && parameter2.equals(pair.parameter2)));
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(parameter1, parameter2);
-    }
-
-    public static <F, S> Pair of(F f, S s) {
-        return new Pair(f, s);
+        int result = 17;
+        result = parameter1 == null ? 31 * result : 31 * result + parameter1.hashCode();
+        result = parameter2 == null ? 31 * result : 31 * result + parameter2.hashCode();
+        return result;
     }
 }
