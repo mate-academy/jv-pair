81142115e0d942d92efeb3035b615eb1c3ec5275
Володимир <matrix.vova@gmail.com>
2020-04-07 07:41:49 +0300

    added solution

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,52 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, S> {
+    private T first;
+    private S second;
 
+    public Pair(T first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public static <T, S> Pair<T, S> of(T first, S second) {
+        return new Pair<>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || !obj.getClass().equals(getClass())) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) obj;
+        return pair.first != null ? pair.first.equals(first) : first == null
+                && pair.second != null ? pair.second.equals(second) : second == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = result * 31 + (first != null ? first.hashCode() : 1);
+        result = result * 31 + (second != null ? second.hashCode() : 1);
+        return result;
+    }
 }
+
+
+
+
+
+
+
+
