578331cf7309f03798e03076354bfc736b58581a
Leonid <rommel@ukr.net>
2020-08-03 16:21:13 +0300

    task done

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,47 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, U> {
+    private final T first;
+    private final U second;
 
+    private Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <T, U> Pair<T, U> of(T first, U second) {
+        return new Pair<>(first, second);
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<?, ?> pair = (Pair<?, ?>) o;
+        if (first == null && pair.first != null
+                || first != null && !first.equals(pair.first)) {
+            return false;
+        }
+        return second == null && pair.second == null
+               || second != null && second.equals(pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = first == null ? 0 : first.hashCode();
+        hash = second == null ? hash : hash * 31 + second.hashCode();
+        return hash;
+    }
 }
