5e5cf9f64a18fb5625ad1c93cda987d2c12b4476
KhOksana99 <kharabara99@gmail.com>
2020-08-04 16:17:17 +0300

    homeWork7.1

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,50 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<F,S> {
+    private F first;
+    private S second;
 
+    private Pair(F first, S second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public F getFirst() {
+        return first;
+    }
+
+    public S getSecond() {
+        return second;
+    }
+
+    public static <F, S> Pair of(F first, S second) {
+        return new Pair(first, second);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (object.getClass().equals(Pair.class)) {
+            if (this == object) {
+                return true;
+            }
+            Pair pair = (Pair) object;
+            return (first == pair.first
+                    || (first != null && first.equals(pair.first)))
+                    && (second == pair.second
+                    || (second != null && second.equals(pair.second)));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        final int number = 11;
+        int result = 5;
+        result = number * result + ((first == null) ? 0 : first.hashCode());
+        result = number * result + ((second == null) ? 0 : second.hashCode());
+        return result;
+    }
 }
