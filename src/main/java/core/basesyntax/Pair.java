9ffa99c7419dd3cd8d8e87db9f84c1d8ee74da10
Ilya <ilya03449@gmail.com>
2020-08-05 00:21:19 +0300

    fixed

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -10,7 +10,7 @@ class Pair<F, S> {
     }
 
     public static <F, S> Pair of(F first, S second) {
-        return new Pair(first, second);
+        return new<F, S> Pair(first, second);
     }
 
     @Override
@@ -21,20 +21,18 @@ class Pair<F, S> {
         if (o == null || !(o.getClass().equals(Pair.class))) {
             return false;
         }
-        Pair<?, ?> pair = (Pair<?, ?>) o;
-        return first == null || pair.first == null
-                ? first == pair.first : first.equals(pair.first)
-                && second == null || pair.second == null
-                ? second == pair.second : second.equals(pair.second);
+        Pair<F, S> pair = (Pair<F, S>) o;
+        return (first == null || pair.first == null
+                ? first == pair.first : first.equals(pair.first))
+                && (second == null || pair.second == null
+                ? second == pair.second : second.equals(pair.second));
     }
 
     @Override
     public int hashCode() {
         int result = 31;
-        result = result * 17 + (first == null ? 0 : first instanceof Number
-                ? ((Number) first).intValue() : first.hashCode());
-        result = result * 17 + (second == null ? 0 : second instanceof Number
-                ? ((Number) second).intValue() : second.hashCode());
+        result = result * 17 + (first == null ? 0 : first.hashCode());
+        result = result * 17 + (second == null ? 0 : second.hashCode());
         return result;
     }
 
