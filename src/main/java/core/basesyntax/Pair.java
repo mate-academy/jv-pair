d9432b890e40dd899043b329aaf98b4580520d24
Leonid <rommel@ukr.net>
2020-08-04 14:32:48 +0300

    equels() changed

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -30,12 +30,8 @@ class Pair<T, U> {
             return false;
         }
         Pair<?, ?> pair = (Pair<?, ?>) o;
-        if (first == null && pair.first != null
-                || first != null && !first.equals(pair.first)) {
-            return false;
-        }
-        return second == null && pair.second == null
-               || second != null && second.equals(pair.second);
+        return (first == pair.first || (first != null && first.equals(pair.first)))
+               && (second == pair.second || (second != null && second.equals(pair.second)));
     }
 
     @Override
