0ffe040cd3cb5f67461e138cfeb76316b42653c7
Fedia <fedia.rudiachenko@gmail.com>
2020-08-04 18:17:25 +0300

    added solution

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,17 +1,6 @@
 package core.basesyntax;
 
 class Pair <I, S> {
-
-    public static void main(String[] args) {
-        Pair<Integer, String> pair = Pair.of(1, "hello");
-        Integer i = pair.getFirst(); // 1
-        String s = pair.getSecond(); // "hello"
-
-        Pair<Integer, String> pair2 = Pair.of(1, "hello");
-        boolean mustBeTrue = pair.equals(pair2); // true!
-        boolean mustAlsoBeTrue = pair.hashCode() == pair2.hashCode(); // true!
-    }
-
     private I first;
     private S second;
 
