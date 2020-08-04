2b1472310cad8b0b4d4f3b56b80aaf673b270a47
tarkaiev <tarkaievd@gmail.com>
2020-08-04 17:18:17 +0300

    added solution

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -9,7 +9,7 @@ public class Pair<T1, T2> {
         this.t2 = t2;
     }
 
-    public static <T1, T2> Pair of(T1 t1, T2 t2) {
+    public static <T1, T2> Pair<T1, T2> of(T1 t1, T2 t2) {
         return new Pair<>(t1, t2);
     }
 
