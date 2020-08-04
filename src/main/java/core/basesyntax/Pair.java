cdbec66820ee8bdc4550301cb522352f44bc598e
AnaLanda <56306772+AnaLanda@users.noreply.github.com>
2020-08-04 15:31:33 +0300

    added solution

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,52 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T> {
+    private Object firstItem;
+    private Object secondItem;
 
+    private Pair(Object firstItem, Object secondItem) {
+        this.firstItem = firstItem;
+        this.secondItem = secondItem;
+    }
+
+    public Object getFirst() {
+        return firstItem;
+    }
+
+    public Object getSecond() {
+        return secondItem;
+    }
+
+    public static <T> Pair<T> of(Object firstItem, Object secondItem) {
+        return new Pair<T>(firstItem, secondItem);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null) {
+            return false;
+        }
+        if (object.getClass().equals(Pair.class)) {
+            Pair pair = (Pair) object;
+            return firstItem == pair.firstItem
+                    || firstItem != null && firstItem.equals(pair.firstItem)
+                     && secondItem == pair.secondItem
+                    || secondItem != null && secondItem.equals(pair.secondItem);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        if (firstItem != null && secondItem != null) {
+            int result = 17;
+            result = 31 * result + firstItem.hashCode();
+            result = 31 * result + secondItem.hashCode();
+            return result;
+        }
+        return 0;
+    }
 }
