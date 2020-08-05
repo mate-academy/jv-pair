50b8ac74bfc1d54bd27cbacff46ff4a87465fccf
Andriy <andriy.lagun@gmail.com>
2020-08-05 09:31:30 +0300

    Implemented Pair<K,V> class

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,44 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<K, V> {
+    private K first;
+    private V second;
 
+    private Pair(K first, V second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static <K, V> Pair of(K first, V second) {
+        return new Pair<>(first, second);
+    }
+
+    public K getFirst() {
+        return first;
+    }
+
+    public V getSecond() {
+        return second;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Pair<K, V> pair = (Pair<K, V>) o;
+        return (first == pair.first || (first != null && first.equals(pair.first)))
+                && (second == pair.second || (second != null && second.equals(pair.second)));
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 11;
+        result = 31 * result + (first == null ? 0 : first.hashCode());
+        result = 31 * result + (second == null ? 0 : second.hashCode());
+        return result;
+    }
 }
