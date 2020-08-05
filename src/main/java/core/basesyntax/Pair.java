d96c924b85d6855534daad87194646aedf75616d
m.mikhajlov <lizappy31@gmail.com>
2020-08-05 15:00:47 +0300

    refactoring

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -17,7 +17,7 @@ public class Pair<F, S> {
         return second;
     }
 
-    public static <K, V> Pair<K, V> of(K first, V second) {
+    public static <K, V> Pair of(K first, V second) {
         return new Pair(first, second);
     }
 
@@ -32,7 +32,7 @@ public class Pair<F, S> {
         if (obj.getClass() != obj.getClass()) {
             return false;
         }
-        Pair<?, ?> pair = (Pair<?, ?>) obj;
+        Pair<F, S> pair = (Pair) obj;
         return first == null ? first == pair.first : first.equals(pair.first)
                 && second == null ? second == pair.second : second.equals(pair.second);
     }
