b561d34a453effd8983ccaf389b493737ba10897
Oleh Kravets <oleg.kravets92@gmail.com>
2020-12-07 13:09:20 +0200

    hashCode was implemented, signatures of equals and toString added

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -20,4 +20,26 @@ class Pair <F, S>{
     public static <F, S> Pair of(F first, S second){
         return new Pair(first, second);
     }
+
+    @Override
+    public int hashCode() {
+        int result = 0;
+        result += getFirst() == null ? 0 : 31 * getFirst().hashCode();
+        result *= getSecond() == null ? 1 : 31 * getSecond().hashCode();
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" +
+                "first=" + first +
+                ", second=" + second +
+                '}';
+    }
 }
