fccecc3141a998b425712d4e6c35c8b71f3c20a1
Volodymyr Mazuryk <mzrkvolodymyr@gmail.com>
2020-12-08 13:40:34 +0200

    implement class Pair(add methods get(), of(), hashCode(), equals());

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,50 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, K> {
+    private T first;
+    private K second;
 
+    private Pair(T first, K second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public K getSecond() {
+        return second;
+    }
+
+    public static <T, K> Pair<T, K> of(T first, K second) {
+        return new Pair<>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object pair) {
+        if (this == pair) {
+            return true;
+        }
+        if (pair == null) {
+            return false;
+        }
+        if (this.getClass().equals(pair.getClass())) {
+            Pair<T, K> current = (Pair<T, K>) pair;
+            if ((first == current.first || first != null && first.equals(current.first))
+                    && (second == current.second || second != null
+                    && second.equals(current.second))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 13;
+        result = 19 * result + (first == null ? 0 : first.hashCode());
+        result = 19 * result + (second == null ? 0 : second.hashCode());
+        return result;
+    }
 }
