a2c32ad3247642d74836caa51455f6cbef6ad834
ShemsheyMaxim <shanhaymax@gmail.com>
2020-12-08 16:06:22 +0200

    add realised class Pair with generics

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,49 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<T, K> {
+    private final T first;
+    private final K second;
 
+    private Pair(T first, K second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public K getSecond() {
+        return second;
+    }
+
+    public static <T, K> Pair<T, K> of(T first, K second) {
+        return new Pair(first, second);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 19;
+        result = 31 * result + (first == null ? 0 : first.hashCode());
+        result = 31 * result + (second == null ? 0 : second.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object pair) {
+        if (this == pair) {
+            return true;
+        }
+        if (pair == null) {
+            return false;
+        }
+        if (pair.getClass().equals(Pair.class)) {
+            Pair<T, K> current = (Pair<T, K>) pair;
+            return (first == current.first
+                    || first != null && first.equals(current.first))
+                    && (second == current.second
+                    || second != null && second.equals(current.second));
+        }
+        return false;
+    }
 }
