6edf45fdf7b9deafa7ba7fd18ab29b6174590354
Alekearl <alexandrhurko@gmail.com>
2020-12-08 20:49:00 +0200

    refactored class Pair

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,34 +1,32 @@
 package core.basesyntax;
 
-import java.util.Objects;
-
 class Pair<T, V> {
-    private T firstPair;
-    private V secondPair;
+    private T first;
+    private V second;
 
-    private Pair(T firstPair, V secondPair) {
-        this.firstPair = firstPair;
-        this.secondPair = secondPair;
+    private Pair(T first, V second) {
+        this.first = first;
+        this.second = second;
     }
 
     public T getFirst() {
-        return firstPair;
+        return first;
     }
 
-    public void setFirst(T firstPair) {
-        this.firstPair = firstPair;
+    public void setFirst(T firstP) {
+        this.first = first;
     }
 
     public V getSecond() {
-        return secondPair;
+        return second;
     }
 
-    public void setSecond(V secondPair) {
-        this.secondPair = secondPair;
+    public void setSecond(V second) {
+        this.second = second;
     }
 
-    public static <T, V> Pair<T, V> of(T firstPair, V secondPair) {
-        return new Pair<>(firstPair, secondPair);
+    public static <T, V> Pair<T, V> of(T first, V second) {
+        return new Pair<>(first, second);
     }
 
     @Override
@@ -36,24 +34,22 @@ class Pair<T, V> {
         if (this == object) {
             return true;
         }
-        if (object == null || getClass() != object.getClass()) {
+        if (object == null) {
             return false;
         }
-
-        Pair<?, ?> pair = (Pair<?, ?>) object;
-
-        if (!Objects.equals(firstPair, pair.firstPair)) {
-            return false;
+        if (object.getClass().equals(Pair.class)) {
+            Pair<T, V> pair = (Pair<T, V>) object;
+            return (pair.first == first || first != null && first.equals(pair.first))
+                    && (pair.second == second || second != null && second.equals(pair.second));
         }
-        return Objects.equals(secondPair, pair.secondPair);
+        return false;
     }
 
     @Override
     public int hashCode() {
         int result = 17;
-        result = 31 * result + (firstPair == null ? 0 : firstPair.hashCode());
-        result = 31 * result + (secondPair == null ? 0 : secondPair.hashCode());
+        result = 31 * result + (first == null ? 0 : first.hashCode());
+        result = 31 * result + (second == null ? 0 : second.hashCode());
         return result;
     }
 }
-
