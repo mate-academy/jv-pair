337216ea570007c7c879d950d35d3bc2c914b905
ilya-brinchuk <ilya.brinchuk@gmail.com>
2020-12-08 15:01:54 +0200

    added generic class Pair in class Pair override method hashCode() and equals()

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,59 @@
 package core.basesyntax;
 
-class Pair {
+class Pair<A, B> {
+    Object first;
+    Object second;
 
+    private Pair(Object first, Object second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public static Pair of(Object a, Object b) {
+        return new Pair(a, b);
+    }
+
+    public Object getFirst() {
+        return first;
+    }
+
+    public void setFirst(Object first) {
+        this.first = first;
+    }
+
+    public Object getSecond() {
+        return second;
+    }
+
+    public void setSecond(Object second) {
+        this.second = second;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * (first == null ? 0 : first.hashCode())
+                + 31 * (second == null ? 0 : second.hashCode());
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object == null) {
+            return false;
+        }
+        if (object == this) {
+            return true;
+        }
+        if (object.getClass().equals(Pair.class)) {
+            Pair pair = (Pair) object;
+            if ((pair.first == null && pair.second == null)
+                    && (this.first != null || this.second != null)) {
+                return false;
+            }
+            return ((pair.first == null && this.first == null)
+                    || pair.first.equals(this.first))
+                    && ((pair.second == null && this.second == null)
+                    || pair.second.equals(this.second));
+        }
+        return false;
+    }
 }
