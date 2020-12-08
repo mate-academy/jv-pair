c323eb52c55aae495e42c9d29258b67d68d35822
Alekearl <alexandrhurko@gmail.com>
2020-12-08 16:17:10 +0200

    created class Pair

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,59 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.Objects;
 
+class Pair<T, V> {
+    private T firstPair;
+    private V secondPair;
+
+    private Pair(T firstPair, V secondPair) {
+        this.firstPair = firstPair;
+        this.secondPair = secondPair;
+    }
+
+    public T getFirst() {
+        return firstPair;
+    }
+
+    public void setFirst(T firstPair) {
+        this.firstPair = firstPair;
+    }
+
+    public V getSecond() {
+        return secondPair;
+    }
+
+    public void setSecond(V secondPair) {
+        this.secondPair = secondPair;
+    }
+
+    public static <T, V> Pair<T, V> of(T firstPair, V secondPair) {
+        return new Pair<>(firstPair, secondPair);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null || getClass() != object.getClass()) {
+            return false;
+        }
+
+        Pair<?, ?> pair = (Pair<?, ?>) object;
+
+        if (!Objects.equals(firstPair, pair.firstPair)) {
+            return false;
+        }
+        return Objects.equals(secondPair, pair.secondPair);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 17;
+        result = 31 * result + (firstPair == null ? 0 : firstPair.hashCode());
+        result = 31 * result + (secondPair == null ? 0 : secondPair.hashCode());
+        return result;
+    }
 }
+
