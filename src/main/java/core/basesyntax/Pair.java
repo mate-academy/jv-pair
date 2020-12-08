52a3f1561a5176af80f9b0628741d849c8d62349
Likh-Alex <likhachov991@protonmail.com>
2020-12-08 15:05:03 +0200

    Implemented Pair class

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,50 @@
 package core.basesyntax;
 
-class Pair {
+public class Pair<T, E> {
+    private T first;
+    private E second;
 
+    private Pair(T first, E second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public E getSecond() {
+        return second;
+    }
+
+    public static <T, E> Pair<T, E> of(T first, E second) {
+        return new Pair<>(first, second);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) {
+            return true;
+        }
+        if (object == null) {
+            return false;
+        }
+        if (getClass().equals(object.getClass())) {
+
+            Pair<?, ?> pair = (Pair<?, ?>) object;
+            return ((this.first == pair.first
+                || this.first != null && this.first.equals(pair.first)))
+                && ((this.second == pair.second
+                || this.second != null && this.second.equals(pair.second)));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 43;
+        result = 53 * result + (first == null ? 0 : first.hashCode());
+        result = 53 * result + (second == null ? 0 : second.hashCode());
+        return result;
+    }
 }
