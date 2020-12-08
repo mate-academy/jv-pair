bed3ff8282a11ca23edc918f2561d64229b94572
Vitalik Kopach <apple_air@ukr.net>
2020-12-08 13:48:08 +0200

    implemented methods of(), equals(), hashcode() to use with generic class Pair<T,S>

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -1,5 +1,43 @@
 package core.basesyntax;
 
-class Pair {
+import java.util.Objects;
 
+public class Pair<T,S> {
+    T firstElement;
+    S secondElement;
+
+    private Pair(T firstElement, S secondElement) {
+        this.firstElement = firstElement;
+        this.secondElement = secondElement;
+    }
+
+    public T getFirst() {
+        return firstElement;
+    }
+
+    public S getSecond() {
+        return secondElement;
+    }
+
+    public static <T,S> Pair<T,S> of(T firstElement, S secondElement) {
+        return new Pair<T,S>(firstElement,secondElement);
+    }
+
+    @Override
+    public boolean equals(Object pair) {
+        if (pair != null && this.getClass() == pair.getClass()) {
+            Pair<T,S> current = (Pair<T,S>) pair;
+            return Objects.equals(this.getFirst(), current.getFirst())
+                    && Objects.equals(this.getSecond(), current.getSecond());
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        int prime = 17;
+        prime = 31 * prime + (firstElement == null ? 0 : firstElement.hashCode());
+        prime = 31 * prime + (secondElement == null ? 0 : secondElement.hashCode());
+        return prime;
+    }
 }
