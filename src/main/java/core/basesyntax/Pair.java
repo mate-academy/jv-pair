685ed4821cff9922350f233f2f382b6126b371c7
m.kislukhina <marharita.kislukhina@gmail.com>
2020-12-09 13:49:31 +0100

    fixed

--- src/main/java/core/basesyntax/Pair.java
+++ src/main/java/core/basesyntax/Pair.java
@@ -20,8 +20,8 @@ class Pair<T, W> {
     @Override
     public int hashCode() {
         int result = 17;
-        result = 21 * result + (first == null ? 0 : first.hashCode());
-        result = 21 * result + (second == null ? 0 : second.hashCode());
+        result = 23 * result + (first == null ? 0 : first.hashCode());
+        result = 23 * result + (second == null ? 0 : second.hashCode());
         return result;
     }
 
@@ -30,21 +30,18 @@ class Pair<T, W> {
         if (pair == this) {
             return true;
         }
-        if (pair == null) {
+        if (pair == null || pair.getClass() != Pair.class) {
             return false;
         }
-        if (pair.getClass().equals(Pair.class)) {
-            Pair current = (Pair) pair;
-            return ((first == current.first || first != null
-                    && first.equals(current.first))
-                    && (second == current.second || second != null
-                    && second.equals(current.second)));
-        }
-        return false;
+        Pair<T, W> current = (Pair<T, W>) pair;
+        return ((first == current.first || first != null
+                && first.equals(current.first))
+                && (second == current.second || second != null
+                && second.equals(current.second)));
     }
 
     public static <T, W> Pair<T, W> of(T first, W second) {
-        return new Pair<T, W>(first, second);
+        return new Pair<>(first, second);
     }
 }
 
