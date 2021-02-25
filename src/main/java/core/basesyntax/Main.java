850b7efc17f3cdbd304bb31c3bffa7739b049746
Tetiana Chupika <mtv1301@gmail.com>
2021-02-25 13:26:03 +0200

    added solution

---
+++ src/main/java/core/basesyntax/Main.java
@@ -0,0 +1,9 @@
+package core.basesyntax;
+
+public class Main {
+    public static void main(String[] args) {
+        String name = "Tetiana";
+        Integer age = 29;
+        Pair<String, Integer> firstPair = Pair.of(name, age);
+    }
+}
